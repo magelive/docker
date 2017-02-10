d24ff15eac20f65f4f2c0bc67fc4c6d1400616dd
Jiang Xin <jiangxin1@xiaomi.com>
2017-02-10 16:43:55 +0800

    modify nginx conf

--- gitblog/run.sh
+++ gitblog/run.sh
@@ -61,29 +61,43 @@ echo "/usr/sbin/nginx" >> ./start.sh
 #build nginx conf default
 
 echo "server {" > ./default
-echo "	listen 80 default_server;">>./default
-echo "	listen [::]:80 default_server;">>./default
-
-echo "	root /var/www/gitblog;">>./default
-
-echo "	index index.php index.html index.htm index.nginx-debian.html;">>./default
-
-echo "	server_name _;">>./default
-
-echo "	location / {">>./default
-echo "		try_files \$uri \$uri/ =404;">>./default
-echo "	}">>./default
-
-echo "	location ~ \\.php$ {">>./default
-echo "		include snippets/fastcgi-php.conf;">>./default
-echo "		fastcgi_pass unix:/run/php/php7.0-fpm.sock;">>./default
-echo "	}">>./default
-
-echo "	location ~ /\\.ht {">>./default
-echo "		deny all;">>./default
-echo "	}">>./default
-echo "}">>./default
-
+echo "	listen 80 default_server;" >> ./default
+echo "	listen [::]:80 default_server;" >> ./default
+echo "	root /var/www/gitblog/;" >> ./default
+echo "	index index.php index.html index.htm index.nginx-debian.html;" >> ./default
+echo "	server_name _;" >> ./default
+echo "	location / {" >> ./default
+echo "		try_files \$uri \$uri/ =404;" >> ./default
+echo "		if (!-e \$request_filename) {" >> ./default
+echo "			rewrite ^(.*)$ /index.php?\$1 last ;" >> ./default
+echo "			break;" >> ./default
+echo "		}" >> ./default
+echo "	}" >> ./default
+echo "	location ~ \\.php$ {" >> ./default
+echo "		include snippets/fastcgi-php.conf;" >> ./default
+echo "		fastcgi_pass unix:/run/php/php7.0-fpm.sock;" >> ./default
+echo "	}" >> ./default
+echo "	location ~ \\.(jpg|png|gif|js|css|swf|flv|ico)$ {" >> ./default
+echo "		expires 12h;" >> ./default
+echo "	}" >> ./default
+echo "	location ~* ^/(doc|logs|app|sys)/ {" >> ./default
+echo "		return 403;" >> ./default
+echo "	}" >> ./default
+echo "	location ~ .*\\.(php|php5|php7)?$" >> ./default
+echo "	{" >> ./default
+echo "		fastcgi_connect_timeout 300;" >> ./default
+echo "		fastcgi_send_timeout 300;" >> ./default
+echo "		fastcgi_read_timeout 300;" >> ./default
+echo "		fastcgi_pass   127.0.0.1:9000;" >> ./default
+echo "		fastcgi_index  index.php;" >> ./default
+echo "		fastcgi_param  SCRIPT_FILENAME \$document_root\$fastcgi_script_name;" >> ./default
+echo "		include	fastcgi_params;" >> ./default
+echo "	}" >> ./default
+echo "	location ~ /\\.ht {" >> ./default
+echo "		deny all;" >> ./default
+echo "	}" >> ./default
+echo "}" >> ./default
+echo >>./default
 
 docker build -t gitblog .
 
@@ -92,3 +106,4 @@ mf=`readlink -f $MDfile_dir`
 docker run -p $port:80 -v $mf:/var/www/gitblog/blog/myblog -d gitblog /start.sh
 
 rm ./default ./Dockerfile ./start.sh
+
